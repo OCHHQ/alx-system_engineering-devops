a2cc2367ea2815b9ffe16ba39b2547fdd1b30441
OCHHQ <enosejecollins@gmail.com>
2024-07-03 23:55:38 -0700

    Let’s practice using Puppet to make changes to our configuration file.

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+content  => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
