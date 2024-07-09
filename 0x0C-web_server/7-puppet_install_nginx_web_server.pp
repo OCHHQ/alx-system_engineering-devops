daaab39c8365374810b5d27440eb8a1800fc559d
OCHHQ <enosejecollins@gmail.com>
2024-07-09 03:31:49 -0700

    puppet install

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,67 @@
-# automating config with puppet
-
-# install Nginx with puppet
+# Ensure the Nginx package is installed
 package { 'nginx':
-  ensure => installed
+  ensure => installed,
 }
 
-file_line { 'install':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
+# Ensure the Nginx service is running and enabled to start on boot
 service { 'nginx':
-  ensure  => running,
-  require => package['nginx'],
+  ensure     => running,
+  enable     => true,
+  require    => Package['nginx'],
+}
+
+# Create the index.html file with "Hello World!" content
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Create the custom 404 page with the required content
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+}
+
+# Manage the Nginx default site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create the template directory if it doesn't exist
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates':
+  ensure => directory,
+}
+
+# Template for the Nginx default site configuration
+file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
+  ensure  => file,
+  content => @(END),
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/@camprollcollins.tech;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+}
+| END
 }
