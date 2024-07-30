9ca03f2ad9df9353d41d469681660a25b0ff9b71
Collins Enoseje <enosejecollins@gmail.com>
2024-07-30 14:02:52 +0100

    Add script to export TODO list data to CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Python script to export employee TODO list data to CSV format
 """
@@ -6,19 +7,24 @@ import csv
 import requests
 import sys
 
+
 def fetch_data(user_id):
     url = 'https://jsonplaceholder.typicode.com/'
     user = requests.get(f'{url}users/{user_id}').json()
     tasks = requests.get(f'{url}todos?userId={user_id}').json()
-    return user['username'], [(user_id, user['username'], task['completed'], task['title']) for task in tasks]
+    return user['username'], [
+        (user_id, user['username'], task['completed'], task['title'])
+        for task in tasks
+    ]
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
-    
+
     user_id = sys.argv[1]
     username, tasks = fetch_data(user_id)
-    
+
     with open(f'{user_id}.csv', 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         writer.writerows(tasks)
