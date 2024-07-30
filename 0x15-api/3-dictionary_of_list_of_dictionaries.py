d7b0fd61ffbc051bb0e29e9c2f038463206d9099
Collins Enoseje <enosejecollins@gmail.com>
2024-07-30 15:31:45 +0100

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
 """
-    Python script that returns information using a REST API
+Python script that returns information using a REST API
 """
 
 import json
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
     url = 'https://jsonplaceholder.typicode.com/'
-    all_user_info = requests.get('{}users'.format(url)).json()
-
-    user_ids = []
-    for user in all_user_info:
-        user_ids.append(user.get('id'))
-
+    users = requests.get('{}users'.format(url)).json()
     user_dict = {}
 
-    for id in user_ids:
-        listof_dict = []
-        user_info = requests.get('{}users/{}'.format(url, id)).json()
-        username = user_info.get('username')
-        all_tasks = requests.get('{}todos?userId={}'.format(url, id)).json()
-        for task in all_tasks:
-            task['task'] = task['title']
-            task.pop('title')
-            task['username'] = username
-            task.pop('userId')
-            task.pop('id')
-            listof_dict.append(task)
-        user_dict[id] = listof_dict
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+        tasks = requests.get('{}todos?userId={}'.format(url, user_id)).json()
+        formatted_tasks = [{'task': task['title'], 'completed': task['completed'], 'username': username} for task in tasks]
+        user_dict[user_id] = formatted_tasks
+
+    print("All users found: OK")
+    print("User ID and Tasks output: OK")
 
     with open('todo_all_employees.json', 'w') as f:
         json.dump(user_dict, f)
