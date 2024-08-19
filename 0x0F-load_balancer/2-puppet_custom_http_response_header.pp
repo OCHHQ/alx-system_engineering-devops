989bdb8828cbd1f348a4265e8ff17632588767f2
Collins Enoseje <enosejecollins@gmail.com>
2024-08-19 05:28:16 +0100

    load balace

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,41 +1,8 @@
-# Configure Nginx with a custom HTTP header (X-Served-By)
-
-# Ensure Nginx is installed
-package { 'nginx':
-  ensure => installed,
-}
-
-# Configure custom HTTP header in Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    server_name _;
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-        root /var/www/html;
-        default_type text/html;
-        return 404 'Ceci n\'est pas une page';
-    }
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-",
-  require => Package['nginx'],
-}
-
-# Restart Nginx service to apply changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'sudo apt-get -y update;
+  sudo apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
 }
